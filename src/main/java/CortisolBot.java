325b96dd36f6be37f85b54aa1d45546ed501b287
PoonnatW <poonnat2006@gmail.com>
2026-08-26 21:36:43 +0800

    refactor: refactor into Task class

--- src/main/java/CortisolBot.java
+++ src/main/java/CortisolBot.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class CortisolBot {
+
+    public static void printLine() {
+        System.out.println("-".repeat(55));
+    }
+
     public static void main(String[] args) {
         String banner = "  ____           _   _           _ ____        _   \n"
                 + " / ___|___  _ __| |_(_)___  ___ | | __ )  ___ | |_ \n"
@@ -9,25 +14,23 @@ public class CortisolBot {
                 + " \\____\\___/|_|   \\__|_|___/\\___/|_|____/ \\___/ \\__|";
         String name = "CortisolBot";
         System.out.println(banner);
-        System.out.println("-".repeat(55));
+        printLine();
         System.out.printf("Greetings sir/madam, %s humbly at your service.\n",name);
         System.out.println("How may I serve you at this evening?");
-        System.out.println("-".repeat(55));
+        printLine();
 
-        String[] tasks = new String[100];
-        String[] tasksStatus = new String[100];
-
+        Task[] tasks = new Task[100];
         int tasksAdded = 0;
         Scanner scanner = new Scanner(System.in);
         while(true) {
             String userInput = scanner.nextLine();
-            System.out.println("-".repeat(55));
+            printLine();
             if (userInput.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int i=0;i<tasksAdded;i++) {
-                    System.out.printf("%d.[%s] %s\n",i+1,tasksStatus[i],tasks[i]);
+                    System.out.printf("%d.%s\n",i+1,tasks[i]);
                 }
-                System.out.println("-".repeat(55));
+                printLine();
             }
             else if (userInput.equals("bye")) {
                 break;
@@ -35,28 +38,27 @@ public class CortisolBot {
             else if (userInput.startsWith("mark ")) {
                 String[] words = userInput.split("\\s+");
                 int taskIndex = Integer.parseInt(words[1]) - 1;
-                tasksStatus[taskIndex] = "X";
+                tasks[taskIndex].markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.printf("\t[%s] %s\n",tasksStatus[taskIndex],tasks[taskIndex]);
-                System.out.println("-".repeat(55));
+                System.out.printf("\t%s\n",tasks[taskIndex]);
+                printLine();
             }
             else if (userInput.startsWith("unmark ")) {
                 String[] words = userInput.split("\\s+");
                 int taskIndex = Integer.parseInt(words[1]) - 1;
-                tasksStatus[taskIndex] = "";
+                tasks[taskIndex].markAsNotDone();
                 System.out.println("Ok, I've marked this task as not done yet: ");
-                System.out.printf("\t[%s] %s\n",tasksStatus[taskIndex],tasks[taskIndex]);
-                System.out.println("-".repeat(55));
+                System.out.printf("\t%s\n",tasks[taskIndex]);
+                printLine();
             }
             else {
-                tasks[tasksAdded] = userInput;
-                tasksStatus[tasksAdded] = "";
+                tasks[tasksAdded] = new Task(userInput);
                 System.out.printf("added: %s\n", userInput);
-                System.out.println("-".repeat(55));
+                printLine();
                 tasksAdded++;
             }
         }
         System.out.println("Tonight has been an honour. I shall bid thee farewell!");
-        System.out.println("-".repeat(55));
+        printLine();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return (isDone ? "[X] " : "[ ] ") + description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
