3fdc4d29279c39b8ceedc02b57a72f5df9b06fdf
PoonnatW <poonnat2006@gmail.com>
2026-08-26 18:28:11 +0800

    feat: rename Duke to CortisolBot

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# CortisolBot User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/CortisolBot.java
@@ -0,0 +1,11 @@
+public class CortisolBot {
+    public static void main(String[] args) {
+        String banner = "  ____           _   _           _ ____        _   \n"
+                + " / ___|___  _ __| |_(_)___  ___ | | __ )  ___ | |_ \n"
+                + "| |   / _ \\| '__| __| / __|/ _ \\| |  _ \\ / _ \\| __|\n"
+                + "| |__| (_) | |  | |_| \\__ \\ (_) | | |_) | (_) | |_ \n"
+                + " \\____\\___/|_|   \\__|_|___/\\___/|_|____/ \\___/ \\__|";
+        String name = "CortisolBot";
+        System.out.println(banner);
+    }
+}
