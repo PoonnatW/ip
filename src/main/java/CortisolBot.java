c7bb156cfaa48eab9931cb885a951f53910ac3fd
PoonnatW <poonnat2006@gmail.com>
2026-08-26 18:44:45 +0800

    feat: add command echoing

--- src/main/java/CortisolBot.java
+++ src/main/java/CortisolBot.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CortisolBot {
     public static void main(String[] args) {
         String banner = "  ____           _   _           _ ____        _   \n"
@@ -11,6 +13,18 @@ public class CortisolBot {
         System.out.printf("Greetings sir/madam, %s humbly at your service.\n",name);
         System.out.println("How may I serve you at this evening?");
         System.out.println("-".repeat(55));
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String userInput = scanner.nextLine();
+            System.out.println("-".repeat(55));
+            if (userInput.equals("bye")) {
+                break;
+            }
+            else {
+                System.out.println(userInput);
+                System.out.println("-".repeat(55));
+            }
+        }
         System.out.println("Tonight has been an honour. I shall bid thee farewell!");
     }
 }
